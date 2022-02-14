0465ea9e75a86624298f4840a430230f1435e1d9
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-02-14 22:24:08 +0100

    Small changes in header card structure
    -delete unnecessaryp element
    -add an extra class to the a element(related with css)
    -change mail in mailto

--- fcc-personal-card/src/CardHeader.js
+++ fcc-personal-card/src/CardHeader.js
@@ -5,14 +5,14 @@ function CardHeader() {
       <img src={photoSrc} className="cardHeader__photo" alt="photo of myself" />
       <h1 className="cardHeader__name">Michał Bysiewicz</h1>
       <p className="cardHeader__info">(Future) Front-end developer</p>
-      <p className="cardHeader__info cardHeader__info--smaller">
-        michal.bysiewicz.kontakt@gmail.com
-      </p>
-      <a className="mailto" href="mailto: bysiewiczmichal@onet.pl">
+      <a
+        className="cardHeader__link mailto"
+        href="mailto: michal.bysiewicz.kontakt@gmail.com"
+      >
         Email
       </a>
       <a
-        className="linkedin"
+        className="cardHeader__link linkedin"
         href="https://www.linkedin.com/in/micha%C5%82-bysiewicz-b02a78231/"
         target="_blank"
       >
