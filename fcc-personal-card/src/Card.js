b06be4801ca78bfb3fb4105375964d82b7c117fe
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-02-15 13:40:20 +0100

    Content for footer

--- fcc-personal-card/src/Card.js
+++ fcc-personal-card/src/Card.js
@@ -1,10 +1,12 @@
 import CardHeader from "./CardHeader";
 import CardMain from "./CardMain";
+import CardFooter from "./CardFooter";
 function Card() {
   return (
     <div className="card">
       <CardHeader />
       <CardMain />
+      <CardFooter />
     </div>
   );
 }
