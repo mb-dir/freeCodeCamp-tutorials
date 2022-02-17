58bd90a7a8c65d7edb0314a0815df4311bdca1f5
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-02-17 22:45:24 +0100

    Adapt Card component for using props
    Now the Card is not hard-coded component

--- fcc-travel-journal/src/components/Card.js
+++ fcc-travel-journal/src/components/Card.js
@@ -1,34 +1,32 @@
 import { GoLocation } from "react-icons/go";
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="card__imgContainer">
         <img
-          src="https://images.unsplash.com/photo-1589308078059-be1415eab4c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          src={props.cardInfo.imageUrl}
           className="card__img"
-          alt=""
+          alt={`Photo of ${props.cardInfo.title}`}
         />
       </div>
       <div className="card__description">
         <header className="card__header">
           <p className="card__countryInfo">
-            <GoLocation className="card__icon" /> Japan
+            <GoLocation className="card__icon" /> {props.cardInfo.location}
             <a
-              href="https://goo.gl/maps/1DGM5WrWnATgkSNB8"
+              href={props.cardInfo.googleMapsUrl}
               className="card__link"
               target="_blank"
             >
               View in google maps
             </a>
           </p>
-          <h2 className="card__visitedPlace">Mount Fuji</h2>
-          <p className="card__visitDate">12 Jan, 2021 - 24 Jan, 2021</p>
+          <h2 className="card__visitedPlace">{props.cardInfo.title}</h2>
+          <p className="card__visitDate">
+            {props.cardInfo.startDate} - {props.cardInfo.endDate}
+          </p>
         </header>
-        <p className="card__info">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <p className="card__info">{props.cardInfo.description}</p>
       </div>
     </div>
   );
