7748d2191e274244c8c4f0462698f4eae3ac0632
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-02-17 22:09:25 +0100

    Main component - wrapper for cards
    This component is like a wrapper for Card components

--- fcc-travel-journal/src/App.js
+++ fcc-travel-journal/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-
+import Main from "./components/Main";
 function App() {
   return (
     <React.StrictMode>
       <Header />
+      <Main />
     </React.StrictMode>
   );
 }
---
+++ fcc-travel-journal/src/components/Main.js
@@ -0,0 +1,10 @@
+import data from "../data.js";
+import Card from "./Card.js";
+function Main() {
+  return (
+    <main className="main">
+      <Card />
+    </main>
+  );
+}
+export default Main;
