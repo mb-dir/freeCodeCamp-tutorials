bf06799eeda9697e89923f71285c26329100092d
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-02-17 22:43:04 +0100

    Add data.js file with some informations
    Based on this data this app will create the right ammounts of Card

---
+++ fcc-travel-journal/src/data.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1617153548707-dfe846085669?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80",
+  },
+  {
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://goo.gl/maps/xAwXG6H2zXe2BorC8",
+    startDate: "02 Jan, 2019",
+    endDate: "14 Jan, 2019",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th century architecture.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1549180030-48bf079fb38a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+  },
+  {
+    title: "Geirangerfjorden",
+    location: "Norway",
+    googleMapsUrl: "https://goo.gl/maps/tr3b3UYtNbDbW5zN7",
+    startDate: "02 Mar, 2017",
+    endDate: "04 Mar, 2017",
+    description:
+      "The fjord is one of Norway's most visited tourist sites. In 2005, it was listed as a UNESCO World Heritage Site, jointly with the Nærøyfjorden. This status was challenged by the disputed plans to build power lines across the fjord.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1558280068-d9fe194ffb0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+];
