dffb0d5557ada5f2fc0a443afe3696a18ae7ebd0
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-03-13 21:45:22 +0100

    Create basic Die component
    In the future there will be 10 such a components, each of them will have
    a special(random) number

--- fcc-tenzies/src/App.js
+++ fcc-tenzies/src/App.js
@@ -1,5 +1,11 @@
+import Die from "./Die";
+
 function App() {
-  return <main className="mainContainer" />;
+  return (
+    <main className="mainContainer">
+      <Die number={1} />
+    </main>
+  );
 }
 
 export default App;
