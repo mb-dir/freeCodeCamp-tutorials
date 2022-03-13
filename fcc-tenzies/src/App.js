625b547fc612a05e7d68bdbcc7874fd236f88ab9
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-03-13 22:24:58 +0100

    Not hardcode Die instances
    Now the Die components are created dynamically based on random numbers -
    thanks to this in which game there will be a different numbers. Also the
    diceNumbers are kept in state - this will be useful for future logic

--- fcc-tenzies/src/App.js
+++ fcc-tenzies/src/App.js
@@ -1,20 +1,25 @@
+import React from "react";
 import Die from "./Die";
 
 function App() {
+  function allNewDice() {
+    const numbers = [];
+    //Return an array(10 elements) with random numbers from 1 to 6
+    for (let i = 0; i < 10; i++) {
+      numbers[i] = Math.floor(Math.random() * (6 - 1)) + 1;
+    }
+    return numbers;
+  }
+
+  //Generate 10 Die components based on generated arrays of random numbers
+  const [ diceNumbers, setDiceNumbers ] = React.useState(allNewDice());
+  const diceList = diceNumbers.map(el => {
+    return <Die number={el} />;
+  });
+
   return (
     <main className="mainContainer">
-      <div className="dieContainer">
-        <Die number={1} />
-        <Die number={5} />
-        <Die number={1} />
-        <Die number={3} />
-        <Die number={2} />
-        <Die number={1} />
-        <Die number={4} />
-        <Die number={3} />
-        <Die number={6} />
-        <Die number={1} />
-      </div>
+      <div className="dieContainer">{diceList}</div>
     </main>
   );
 }
