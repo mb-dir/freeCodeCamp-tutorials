ab0a7afb86b47fb688575ef29ef26f7f7b67b7d1
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-03-15 20:40:49 +0100

    Add game "presentation"

--- fcc-tenzies/src/App.js
+++ fcc-tenzies/src/App.js
@@ -53,6 +53,11 @@ function App() {
 
   return (
     <main className="mainContainer">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dieContainer">{diceList}</div>
       <button className="rollBtn" onClick={roll}>
         Roll
